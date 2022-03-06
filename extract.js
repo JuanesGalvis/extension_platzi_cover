d34575a3a1db8fd1221b4a4ebf890f65d8e48b05
Juan Galvis <juangalvis.sh@gmail.com>
2022-03-06 15:52:47 -0500

    Update Extension - Platzi a unificado la página de los cursos

--- extract.js
+++ extract.js
@@ -4,16 +4,12 @@ let Cover;
 let Icon;
 
 if (Web.includes('cursos')) {
+    
     Cover = document.querySelector('meta[property="og:image"]');
     Icon = document.querySelector('.Hero-badge img');
     console.log(`Icon: ${Icon.src} ~ Cover: ${Cover.content}`);
     
-} else if (Web.includes('clases')) {
-    Cover = document.querySelector('meta[property="og:image"]');
-    Icon = document.querySelector('.CourseDetail-left-figure img');
-    console.log(`Icon: ${Icon.src} ~ Cover: ${Cover.content}`); 
 } else {
     // Validación si se ejecuta la extensión fuera de Platzi
-    console.log("✋🏻 ESTA EXTENSIÓN SOLO FUNCIONA EN CURSOS DE PLATZI 💚")
+    console.log("✋🏻 ESTA EXTENSIÓN SOLO FUNCIONA EN LA PÁGINA DE ALGÚN CURSO DE PLATZI 💚")
 }
-
